f6e06fd62874fae3737dc8921e24191fd7929e2e
T3-Milk <118168271+T3-Milk@users.noreply.github.com>
2022-11-15 11:57:10 +1000

    Update JumpScare.ps1

--- JumpScare.ps1
+++ JumpScare.ps1
@@ -99,7 +99,7 @@ $WallpaperStyle = Switch ($Style) {
 If($Style -eq "Tile") {
  
     New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name WallpaperStyle -PropertyType String -Value $WallpaperStyle -Force
-    New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name TileWallpaper -PropertyType String -Value 10 -Force
+    New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name TileWallpaper -PropertyType String -Value 1 -Force
  
 }
 Else {
